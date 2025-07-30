a024fe1c1a185245aec036efdaa6725a70cff366
Ashishroy1724 <ashishroy78782@gmail.com>
2025-07-30 09:25:39 +0530

    CRUD Operation added comments removed

--- backend/config/Routes/SubRoute.js
+++ backend/config/Routes/SubRoute.js
@@ -13,21 +13,3 @@ router.put('/:id', updateSub);
 router.delete('/:id', deleteSub);
 
 export default router;
-
-// {
-//     "brandLogo": "https://logo.clearbit.com/netflix.com",
-//     "brandName": "Netflix",
-//     "currency": "INR",
-//     "amount": 499,
-//     "frequency": "Monthly",
-//     "startDate": "2025-07-01T00:00:00.000Z",
-//     "endDate": "2026-07-01T00:00:00.000Z",
-//     "note": "OTT streaming",
-//     "user": "68898ee22d1bebb26221530f",
-//     "_id": "6889965facc38b00afab40c7",
-//     "createdAt": "2025-07-30T03:49:51.954Z",
-//     "updatedAt": "2025-07-30T03:49:51.954Z",
-//     "__v": 0
-// }
-
-// 68898ee22d1bebb26221530f
